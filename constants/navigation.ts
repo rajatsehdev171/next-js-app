fcb77fd5e960cd398465a522a91c77a3e3366555
Rajat <rajat@riceracing.com>
2024-07-07 16:34:44 +0530

    constants updated

--- constants/navigation.ts
+++
@@ -1,6 +0,0 @@
-export const navigationRoutes = [
-    { linkVal: 'Home', route: '/' },
-    { linkVal: 'About', route: '/about' },
-    { linkVal: 'Blogs', route: '/blogs' },
-    { linkVal:'SignUp', route: '/signup' },
-];
